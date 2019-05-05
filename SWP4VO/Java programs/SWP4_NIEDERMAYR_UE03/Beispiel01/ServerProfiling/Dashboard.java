77fc3dde9a6603e6d19a6046b1b0ec8169d18650
Ichiro is cool guy <50051321+TrueIchiro@users.noreply.github.com>
2019-05-05 23:40:08 +0200

    Homework git gud
    Adding my goddamn homework
    Barely fuckin touched

---
+++ SWP4VO/Java programs/SWP4_NIEDERMAYR_UE03/Beispiel01/ServerProfiling/Dashboard.java
@@ -0,0 +1,5 @@
+package Beispiel01.ServerProfiling;
+
+//watches over Profilers and takes their values
+public class Dashboard {
+}
---
+++ SWP4VO/Java programs/SWP4_NIEDERMAYR_UE03/Beispiel01/Test/ServerProfilingTest.java
@@ -0,0 +1,9 @@
+package Beispiel01.Test;
+
+public class ServerProfilingTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
